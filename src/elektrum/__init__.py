e1ea2c5d91bb71d04e0bab53f75f514fc752d952
Adam Lamson <lamsoa729@gmail.com>
2025-10-28 15:04:56 +0100

    Split up kinetic model into regular and king_altman

--- src/elektrum/__init__.py
+++ src/elektrum/__init__.py
@@ -1,4 +1,8 @@
-from .neural_network_builder import KineticNeuralNetworkBuilder, KineticEigenModelBuilder
+from .neural_network_builder import (
+    KineticNeuralNetworkBuilder,
+    KineticEigenModelBuilder,
+)
+from .kinetic_model import KineticModel, KingAltmanKineticModel
 
 
 __version__ = "0.0.3"
